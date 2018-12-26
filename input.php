b358f120372a269f505bef4f07221bfca7c18419
Santippello Forense <marco_santippe@hotmail.com>
2018-12-26 01:49:23 +0100

    firstOne

---
+++ input.php
@@ -0,0 +1,11 @@
+<table class="mdl-data-table mdl-js-data-table mdl-cell mdl-cell--8-col ">  
+  <tbody>
+      <tr><td>
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" id="user" pattern="[A-Z,a-z, ]*">
+            <label class="mdl-textfield__label" for="user">User name</label>
+            <span class="mdl-textfield__error">Letters and spaces only</span>
+        </div>
+      </td></tr>
+  </tbody>
+</table>
---
+++ tables.php
@@ -0,0 +1,80 @@
+<table class="mdl-cell mdl-cell--8-col mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp mdl-grid--no-spacing">
+    <thead>
+        <tr>
+        <th class="mdl-data-table__cell--non-numeric">Material</th>
+        <th>Quantity</th>
+        <th>Unit price</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+            <td>25</td>
+            <td>$2.90</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
+            <td>50</td>
+            <td>$1.25</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
+            <td>10</td>
+            <td>$2.35</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+            <td>25</td>
+            <td>$2.90</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
+            <td>50</td>
+            <td>$1.25</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
+            <td>10</td>
+            <td>$2.35</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+            <td>25</td>
+            <td>$2.90</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
+            <td>50</td>
+            <td>$1.25</td>
+        </tr>
+        <tr>
+            <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue)</td>
+            <td>10</td>
+            <td>$2.35</td>
+        </tr>
+    </tbody>
+</table>
+<div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
+    <div class="demo-updates mdl-card__title mdl-card--expand mdl-color--teal-300">
+        <h2 class="mdl-card__title-text">Updates</h2>
+    </div>
+    <div class="mdl-card__supporting-text mdl-color-text--grey-600">
+        Non dolore elit adipisicing ea reprehenderit consectetur culpa.
+    </div>
+    <div class="mdl-card__actions mdl-card--border">
+        <a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect">Read More</a>
+    </div>
+</div>
+<div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--7-col mdl-cell--4-col-tablet mdl-cell--6-col-desktop">
+    <div class="demo-updates mdl-card__title mdl-card--expand mdl-color--teal-300">
+        <h2 class="mdl-card__title-text">Updates</h2>
+    </div>
+    <div class="mdl-card__supporting-text mdl-color-text--grey-600">
+        Non dolore elit adipisicing ea reprehenderit consectetur culpa.
+    </div>
+    <div class="mdl-card__actions mdl-card--border">
+        <a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect">Read More</a>
+    </div>
+</div>
+<div class="demo-separator mdl-cell--1-col"></div>
+    
